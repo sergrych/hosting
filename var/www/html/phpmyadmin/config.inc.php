3ac4f706872f2d90b97193941947fc201810c02e
Serg <sergrych1973@gmail.com>
2022-07-13 17:01:11 +0500

    add blowfish_secret

--- var/www/html/phpmyadmin/config.inc.php
+++ var/www/html/phpmyadmin/config.inc.php
@@ -3,7 +3,7 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'ZaEa75ypx8p9JuoG8fe04GOhSXU1dDAu'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
